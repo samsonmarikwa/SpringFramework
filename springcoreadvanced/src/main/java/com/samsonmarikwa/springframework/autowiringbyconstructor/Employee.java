ec5347d036e5f4bdc530bac57b60abc68850f704
Samson Marikwa <samsonmarikwa@outlook.com>
2022-09-23 13:05:43 -0400

    Autowiring by Constructor

---
+++ springcoreadvanced/src/main/java/com/samsonmarikwa/springframework/autowiringbyconstructor/Employee.java
@@ -0,0 +1,23 @@
+package com.samsonmarikwa.springframework.autowiringbyconstructor;
+
+public class Employee {
+	private Address address;
+
+	public Employee(Address address) {
+		this.address = address;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [address=" + address + "]";
+	}
+
+}
