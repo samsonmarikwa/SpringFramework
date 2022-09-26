3953dfad9391d484a6bad952f3e07544208519c0
Samson Marikwa <samsonmarikwa@outlook.com>
2022-09-26 01:17:00 -0400

    Injecting interfaces using annotations - @Component, @Autowired, @Qualifier

---
+++ springcoreadvanced/src/main/java/com/samsonmarikwa/springframework/injecting/interfacesannotations/OrderBO.java
@@ -0,0 +1,7 @@
+package com.samsonmarikwa.springframework.injecting.interfacesannotations;
+
+public interface OrderBO {
+
+	void placeOrder();
+	
+}
---
+++ springcoreadvanced/src/main/java/com/samsonmarikwa/springframework/injecting/interfacesannotations/OrderDAOImpl.java
@@ -0,0 +1,13 @@
+package com.samsonmarikwa.springframework.injecting.interfacesannotations;
+
+import org.springframework.stereotype.Component;
+
+@Component("dao")
+public class OrderDAOImpl implements OrderDAO {
+
+	@Override
+	public void createOrder() {
+		System.out.println("Inside Order DAO createOrder()");
+	}
+
+}
