1310f3e710359d4cea10dce36e9ec0e7d677bd85
Samson Marikwa <samsonmarikwa@outlook.com>
2022-09-27 23:16:38 -0400

    Create ORM project

---
+++ springorm/src/main/java/com/samsonmarikwa/springframework/springorm/App.java
@@ -0,0 +1,13 @@
+package com.samsonmarikwa.springframework.springorm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
