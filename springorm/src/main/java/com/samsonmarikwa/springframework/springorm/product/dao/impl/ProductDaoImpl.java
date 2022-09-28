1136ce763ef84dfed4b062986f780295d9b67900
Samson Marikwa <samsonmarikwa@outlook.com>
2022-09-28 16:55:58 -0400

    Configure SessionFactory, HibernateTemplate and TransactionManager

--- springorm/src/main/java/com/samsonmarikwa/springframework/springorm/product/dao/impl/ProductDaoImpl.java
+++ springorm/src/main/java/com/samsonmarikwa/springframework/springorm/product/dao/impl/ProductDaoImpl.java
@@ -1,12 +1,16 @@
 package com.samsonmarikwa.springframework.springorm.product.dao.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
 
 import com.samsonmarikwa.springframework.springorm.product.dao.ProductDao;
 import com.samsonmarikwa.springframework.springorm.product.entity.Product;
 
+@Component
 public class ProductDaoImpl implements ProductDao {
 	
+	@Autowired // inject the HibernateTemplate bean created in the config.xml file
 	HibernateTemplate hibernateTemplate;
 
 	@Override
