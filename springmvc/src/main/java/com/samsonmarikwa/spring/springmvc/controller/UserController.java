2bfb17db7227b0954a180ea424e5f551b5b41e51
Samson Marikwa <samsonmarikwa@outlook.com>
2022-10-01 16:30:19 -0400

    Create model / dto, controller with ModelAttribute & JSP file with form

---
+++ springmvc/src/main/java/com/samsonmarikwa/spring/springmvc/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.samsonmarikwa.spring.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.samsonmarikwa.spring.springmvc.dto.User;
+
+@Controller
+public class UserController {
+
+	@RequestMapping("registrationPage")
+	public ModelAndView showRegistrationPage() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("userReg");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value="registerUser", method=RequestMethod.POST)
+	public ModelAndView registerUser(@ModelAttribute("user") User user) {
+		System.out.println(user);
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("userReg");
+		return modelAndView;
+	}
+}
