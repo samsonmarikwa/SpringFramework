a18606af781438618570e449e5c5683aa745ff80
Samson Marikwa <samsonmarikwa@outlook.com>
2022-10-02 22:25:10 -0400

    ModelMap addAttribute and String View

--- springmvc/src/main/java/com/samsonmarikwa/spring/springmvc/controller/UserController.java
+++ springmvc/src/main/java/com/samsonmarikwa/spring/springmvc/controller/UserController.java
@@ -1,29 +1,23 @@
 package com.samsonmarikwa.spring.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.samsonmarikwa.spring.springmvc.dto.User;
 
 @Controller
 public class UserController {
 
 	@RequestMapping("registrationPage")
-	public ModelAndView showRegistrationPage() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("userReg");
-		return modelAndView;
+	public String showRegistrationPage() {
+		return "userReg";
 	}
 	
 	@RequestMapping(value="registerUser", method=RequestMethod.POST)
-	public ModelAndView registerUser(@ModelAttribute("user") User user) {
-		System.out.println(user);
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("user", user);
-		modelAndView.setViewName("regResult");
-		return modelAndView;
+	public String registerUser(@ModelAttribute("user") User user, ModelMap model) {
+		model.addAttribute("user", user);
+		return "regResult";
 	}
 }
