ff618e3d1ced323e23ce2050330cb9b82b7b5e04
Samson Marikwa <samsonmarikwa@outlook.com>
2022-10-04 21:30:54 -0400

    Create the advices

--- springaop/src/main/java/com/samsonmarikwa/springframework/springaop/aspect/LogginAspect.java
+++ springaop/src/main/java/com/samsonmarikwa/springframework/springaop/aspect/LogginAspect.java
@@ -1,8 +1,20 @@
 package com.samsonmarikwa.springframework.springaop.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class LogginAspect {
 
+	@Before("")
+	public void logBefore(JoinPoint joinPoint) {
+		System.out.println("Before Calling the method");
+	}
+
+	@After("")
+	public void logAfter(JoinPoint joinPoint) {
+		System.out.println("After the method execution");
+	}
 }
