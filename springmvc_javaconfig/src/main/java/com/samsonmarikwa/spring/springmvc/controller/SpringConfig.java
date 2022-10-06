e876d72ebb29a025efd27eeb4e97ad896896cd98
Samson Marikwa <samsonmarikwa@outlook.com>
2022-10-06 13:20:17 -0400

    Configure the Dispatcher Servlet InternalViewResolver - we can delete the dispatcher-servlet.xml

--- springmvc_javaconfig/src/main/java/com/samsonmarikwa/spring/springmvc/controller/SpringConfig.java
+++ springmvc_javaconfig/src/main/java/com/samsonmarikwa/spring/springmvc/controller/SpringConfig.java
@@ -4,14 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @ComponentScan("com.samsonmarikwa.spring.springmvc.controller")
 @Configuration
-public class SpringConfig extends WebMvcConfigurerAdapter {
+public class SpringConfig implements WebMvcConfigurer {
 
 	@Bean
 	public ViewResolver viewResolver() {
@@ -19,5 +20,10 @@ public class SpringConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
 	}
 }
