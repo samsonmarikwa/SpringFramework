d0a3279c83d521636618243c5766de1ca4c34ddd
Samson Marikwa <samsonmarikwa@outlook.com>
2022-10-07 12:45:21 -0400

    create dao and service classes and test autowiring and bean creation

--- springboot/src/test/java/com/samsonmarikwa/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/samsonmarikwa/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,22 @@
 package com.samsonmarikwa.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import com.samsonmarikwa.springboot.service.Service;
 
 @SpringBootTest
 class SpringbootApplicationTests {
+	
+	@Autowired
+	ApplicationContext context;
 
 	@Test
-	void contextLoads() {
+	public void testService() {
+		Service service = context.getBean(Service.class);
+		service.save();
 	}
 
 }
